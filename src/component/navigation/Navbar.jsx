40a1899766643b91631782fa30375d36164af191
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-08 19:48:04 +0530

    Update Navbar.jsx

--- src/component/navigation/Navbar.jsx
+++ src/component/navigation/Navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Navbar() {
   return (
-    
+    <div className="navbar">
+        <ul className="navlinks">
+            <li className="active"><a href="#">Home</a></li>
+            <li className="active"><a href="#">Report-us</a></li>
+            <li className="active"><a href="#">Categories</a></li>
+            <li className="active"><a href="#">Hospital</a></li>
+            <li className="active"><a href="#">Contact</a></li>
+            <li className="active"><a href="#">About</a></li>
+        </ul>
+    </div>
   )
 }
 
