03867b85c82dc1692f1988a852082ec0a443e698
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-08 20:00:04 +0530

    Update Navbar.jsx

--- src/component/navigation/Navbar.jsx
+++ src/component/navigation/Navbar.jsx
@@ -3,6 +3,9 @@ import './Navbar.css'
 function Navbar() {
   return (
     <div className="navbar">
+        <ul className="navlogo">
+            <li className="logo"><span style={{ color: 'tomato' }}>S</span>arp<span style={{ color: 'tomato' }}>R</span>esQ</li>
+        </ul>
         <ul className="navlinks">
             <li className="active"><a href="#">Home</a></li>
             <li className="active"><a href="#">Report-us</a></li>
