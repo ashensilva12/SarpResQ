f49ee019139f3598520890e1f59c51e31dc13709
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-09 20:10:54 +0530

    Update Login.jsx

--- src/component/pages/Login.jsx
+++ src/component/pages/Login.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function Login() {
   return (
     <div>
-        
+        <form>
+            <h2>Login</h2>
+            <table>
+                
+            </table>
+        </form>
     </div>
   )
 }
