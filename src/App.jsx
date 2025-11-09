181b3f0f6bdd9c4348264d3a426298264c1c0359
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-09 20:21:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Navbar from './component/navigation/Navbar'
+import Navbar from './component/pages/Login'
+import Login from './component/pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Navbar />
+      <Login/>
     </div>
   )
 }
