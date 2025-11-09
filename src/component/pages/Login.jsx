b83358d72c8f1c3e21e7f5800d3d915afddde75c
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-09 21:33:04 +0530

    Update Login.jsx

--- src/component/pages/Login.jsx
+++ src/component/pages/Login.jsx
@@ -10,14 +10,14 @@ function Login() {
             <table className='table'>
                 <tr>
                     <td>Username:</td>
-                    <td><input type="email" name="username" required/></td>
+                    <td><input type="email" name="username"placeholder="&#9993; Email" required/></td>
                 </tr>
                 <tr>
                     <td>Password:</td>
-                    <td><input type="password" name="password" required/></td>
+                    <td><input type="password" name="password" placeholder="&#128274; Password" required/></td>
                 </tr>
                 <tr>
-                    <td colSpan="2" ><button type="submit">Login</button></td>
+                    <td colSpan="2" ><button type="submit">LOGIN</button></td>
                 </tr>
             </table>
         </form>
