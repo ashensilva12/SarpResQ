77b3734038a407b2295ed1c61fc9c0bada3364ee
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-09 20:21:14 +0530

    Update Login.jsx

--- src/component/pages/Login.jsx
+++ src/component/pages/Login.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import './Login.css'
 function Login() {
   return (
     <div>
         <form>
-            <h2>Login</h2>
-            <table>
+            <h2 className='title'>Login</h2>
+            <table className='table'>
                 <tr>
                     <td>Username:</td>
                     <td><input type="text" name="username" required/></td>
