4c2587f5af2aff7a3d79f9f8f23cba38fbc17ec9
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-12 10:16:06 +0530

    Update Register.jsx

--- src/component/pages/Register.jsx
+++ src/component/pages/Register.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Register() {
   return (
     <div className="loginbox">
-        
+        <div className="loginheader">
+            <h2 className='title'>Register</h2>
+        </div>
     </div>
   )
 }
