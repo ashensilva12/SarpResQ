236d52468cde11519ff2e2e1e263ebec68a8be88
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-12 21:18:56 +0530

    Create Home.jsx

---
+++ src/component/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '../navigation/Navbar'
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+        <div>
+            <Navbar/>
+        </div>
+        <div className='home'>
+            <section className="main">
+                <div className='main_content'>
+                    
+                </div>
+            </section>
+        </div>
+    
+    </div>
+  )
+}
+
+export default Home
