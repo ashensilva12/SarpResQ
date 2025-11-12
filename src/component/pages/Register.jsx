23098f4431b1fda85cb65a501ce443e5be7dacb0
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-12 10:15:38 +0530

    Update Register.jsx

--- src/component/pages/Register.jsx
+++ src/component/pages/Register.jsx
@@ -4,29 +4,6 @@ function Register() {
   return (
     <div className="loginbox">
         
-        <div className="loginheader">
-            <h2 className='title'>Register</h2>
-        </div>
-        <form>
-            <table className='table'>
-                <tr>
-                    <td>Username:</td>
-                    <td><input type="email" name="username"placeholder="&#9993; Email" required/></td>
-                </tr>
-                <tr>
-                    <td>Password:</td>
-                    <td><input type="password" name="password" placeholder="&#128274; Password" required/></td>
-                </tr>
-                <tr>
-                    <td>Confirm Password:</td>
-                    <td><input type="password" name="confirmPassword" placeholder="&#128274; Confirm Password" required/></td>
-                </tr>
-                <tr>
-                    <td colSpan="2" ><button type="submit">Register</button></td>
-                </tr>
-            </table>
-        </form>
-        
     </div>
   )
 }
