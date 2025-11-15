5ee9a0b7853969707edc34e542b906819ba37a0f
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-15 18:50:10 +0530

    Update Register.jsx

--- src/component/pages/Register.jsx
+++ src/component/pages/Register.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from 'react'
+import './Register.css'
 import loginImage from '../../assets/login.jpg';
 import { Link } from 'react-router-dom';
 
 function Register() {
-  return (
-    <div className="loginbox">
-        <div className="loginheader">
-            <h2 className='title'>Register</h2>
-        </div>
+    const [location, setLocation] = useState('')
+
+    const handleSetLocation = () => {
+        if (!navigator.geolocation) {
+            alert('Geolocation is not supported by your browser')
+            return
+        }
+        navigator.geolocation.getCurrentPosition(
+            (pos) => {
+                const lat = pos.coords.latitude.toFixed(6)
+                const lng = pos.coords.longitude.toFixed(6)
+                setLocation(`${lat}, ${lng}`)
+            },
+            (err) => {
+                alert('Unable to retrieve your location')
+            },
+            { enableHighAccuracy: true, timeout: 10000 }
+        )
+    }
+    return (
+        <div className="auth-page">
+            <div className="loginbox">
+                <div className="loginheader">
+                        <h2 className='title'>Register</h2>
+                </div>
         <form>
             <table className='table'>
                 <tr>
@@ -24,14 +44,32 @@ function Register() {
                     <td><input type="password" name="confirmPassword" placeholder="&#128274; Confirm Password" required/></td>
                 </tr>
                 <tr>
+                    <td>Location :</td>
+                    <td>
+                        <div className="location-row">
+                            <input
+                                className="location-input"
+                                type="text"
+                                name="location"
+                                placeholder="&#127968; Location"
+                                value={location}
+                                onChange={(e) => setLocation(e.target.value)}
+                                required
+                            />
+                            <button type="button" className="set-location-btn" onClick={handleSetLocation}>Set Location</button>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
                     <td colSpan="2" ><button type="submit">Register</button></td>
                 </tr>
                 <tr>
                     <td colSpan="2" style={{textAlign: 'center', paddingTop: '12px'}}>Already have an account? <Link to="/">Login</Link></td>
                 </tr>
             </table>
-        </form>
-    </div>
+                </form>
+            </div>
+        </div>
   )
 }
 
