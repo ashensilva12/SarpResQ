191b8723ba3f1dc20393dcbe645cf0f3d9e0efbb
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-15 18:50:27 +0530

    Create Report.jsx

---
+++ src/component/pages/Report.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../navigation/Navbar'
+
+function Report() {
+  return (
+    <div>
+        <div>
+            <Navbar/>
+        </div>
+    </div>
+  )
+}
+
+export default Report
