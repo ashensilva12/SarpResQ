08fa1dbda48fa42fbb73604aba4ba4cd63de82c8
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-29 13:31:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Register from './component/pages/Register'
 import Home from './component/pages/Home'
 import Report from './component/pages/Report'
 import Categories from './component/pages/Categories'
+import Hospital from './component/pages/Hospital'
 import { BrowserRouter as Router,Routes,Route }from 'react-router-dom';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route path="/Home" element={<Home/>} />
           <Route path="/Report" element={<Report/>} />
           <Route path="/Categories" element={<Categories/>} />
+          <Route path="/Hospital" element={<Hospital/>} />
         </Routes>
       </div>    
     </Router>
